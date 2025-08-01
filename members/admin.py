54baafda915548bf923bf9178f8d4625eca351d7
nchiwar <nehechiwar@gmail.com>
2025-08-01 19:23:20 +0100

    FAQ Done

--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import MembershipApplication, Publication
+from .models import FAQ, ContactMessage, Publication
 
-admin.site.register(MembershipApplication)
+admin.site.register(FAQ)
+admin.site.register(ContactMessage)
 admin.site.register(Publication)
