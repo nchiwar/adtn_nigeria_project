9a45f9aa9c312734cb44b007d44c75380c7b87de
nchiwar <nehechiwar@gmail.com>
2025-08-31 16:33:48 +0100

    Changes for Render Deployment

--- adtn_nigeria/settings.py
+++ adtn_nigeria/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--k1&v%!i=%yo-mbb9dwl!%eot+02ljiagr@rq1s&%b(3%@xj%x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -207,5 +207,6 @@ AUTHENTICATION_BACKENDS = [
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 
-
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, default=os.getenv('DATABASE_URL'))
 
