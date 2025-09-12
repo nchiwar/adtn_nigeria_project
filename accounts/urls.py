3b2a7920050e1913461b0b2f68837ee1aa72e381
nchiwar <nehechiwar@gmail.com>
2025-09-12 15:35:33 +0100

    Add payment_view function

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .models import Purchase
+
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
@@ -8,4 +10,7 @@ urlpatterns = [
     path('memberspage/', views.membership_view, name='memberspage'),
     path('forgot-password/', views.forgot_password_view, name='forgot_password'),
     path('about/', views.about_view, name='about'),
+    path('payment/', views.payment_view, name='payment'),
+    path('subscription-success/', views.subscription_success, name='subscription_success'),
+    path('purchase-success/', views.purchase_success, name='purchase_success'),
 ]
